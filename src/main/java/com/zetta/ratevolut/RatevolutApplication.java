ce2abc83048e3881cbd3633d69ff4c74d93ee2c0
Tihomir Kehayov <kehayov89@gmail.com>
2026-06-26 17:33:10 +0300

    init commit

---
+++ src/main/java/com/zetta/ratevolut/RatevolutApplication.java
@@ -0,0 +1,13 @@
+package com.zetta.ratevolut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatevolutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatevolutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zetta/ratevolut/RatevolutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zetta.ratevolut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatevolutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
